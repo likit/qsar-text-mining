50fd4b628fda3903c9e8c8c0579b3491950ac7cc
Likit Preeyanon <preeyano@msu.edu>
2015-07-14 22:52:55 +0700

    updated search query to download data by year
    data is now stored in mongodb

--- src/search.py
+++ src/search.py
@@ -1,33 +1,42 @@
+# coding:utf-8
 import os
 import requests
 import xml.etree.ElementTree as et
+import json
 import sys
 import time
+import pymongo
+import re
 from datetime import datetime
+
+db = pymongo.Connection()['data']
 
 api_key = os.environ['SCOPUS_APIKEY']
 
 '''User defined variables.'''
-query = 'qsar'
-item_per_page = 25
-start_year = 2010
-end_year = 2016
+item_per_page = 200
+pub_year = 2015
+outfile_prefix = 'qsar'
+query = 'tak(qsar) AND pub-date IS %d' % pub_year
+sleeptime = 5
 
 url = 'http://api.elsevier.com/content/search/index:SCIDIR'
 params = {'apiKey': api_key, 'query': query}
 apikey = {'apiKey': api_key}
 
-print >> sys.stderr, 'Downloading papers from year %d to %d' % (start_year, end_year)
-print >> sys.stderr, 'Keyword(s) is "%s"' % (query)
+print >> sys.stderr, 'Downloading papers published in %d' % pub_year
+print >> sys.stderr, 'Query is "%s"' % (query)
 
 r = requests.get(url, params=params)
 total_results = int(r.json()['search-results']['opensearch:totalResults'])
+print >> sys.stderr, 'Total articles = %d' % total_results
 
-page = 1
-for start in range(0,total_results+1, item_per_page)
-    print >> sys.stderr, 'Waiting to download from page %d...' % page
-    time.sleep(5)
-    page += 1
+page = 0
+for start in range(0,total_results+1, item_per_page):
+    print >> sys.stderr, 'Waiting %d sec to download from page %d...' % \
+                                (sleeptime, page+1)
+    time.sleep(sleeptime)
+    # op = open('%s_page_%d_%d' % (outfile_prefix, page+1, pub_year), 'w')
     url = 'http://api.elsevier.com/content/search/index:SCIDIR'
     params = {'apiKey': api_key,
                 'query': query,
@@ -36,29 +45,29 @@ for start in range(0,total_results+1, item_per_page)
 
     articles = requests.get(url, params=params).json()['search-results']
     for n, entry in enumerate(articles['entry']):
+        print >> sys.stderr, '\tdownloading %s...' % entry['dc:title'][:25]
         article_url = 'http://api.elsevier.com/content/article/%s' % (entry['dc:identifier'])
         article = requests.get(article_url, params=apikey)
         content = et.fromstring(article.text.encode('utf-8'))
-        authors = []
-        keywords = []
+        article_dict= {}
         for child in content.getchildren():
             for i in child:
-                if 'creator' in i.tag:
-                    authors.append(i.text.encode('utf8'))
-                elif 'title' in i.tag:
-                    title = i.text.encode('utf8')
-                elif 'description' in i.tag:
-                    desc = i.text.encode('utf8').lstrip('Abstract')
-                elif 'publicationName' in i.tag:
-                    pubname = i.text.encode('utf8')
-                elif 'coverDate' in i.tag:
-                    coverdate = i.text
-                elif 'openaccessArticle' in i.tag:
-                    openaccess_article = i.text
-                elif 'subject' in i.tag:
-                    keywords.append(i.text.encode('utf8'))
+                tag = re.match('(\{([^}]+)\})(.*)',
+                        i.tag.encode('utf-8')).groups()[-1]
+                if tag not in article_dict:
+                    if i.text:
+                        article_dict[tag] = [i.text.encode('utf-8')]
+                    else:
+                        article_dict[tag] = [None]
+                else:
+                    if i.text:
+                        article_dict[tag].append(i.text.encode('utf-8'))
+                    else:
+                        article_dict[tag].append(None)
 
-        coveryear = datetime.strptime(coverdate, '%Y-%m-%d').year
-        if (coveryear >= start_year and coveryear < end_year):
-            print '|'.join(authors) + '\t' + title + '\t' + desc + '\t' + ','.join(keywords) + '\t' + coverdate + '\t' + pubname + '\t' + openaccess_article
-            print >> sys.stderr, n, title[:20]+'...', desc[:40]+'...', coverdate, '|'.join(authors)
+        existing_article = db.scopus.find_one({'identifier': article_dict['identifier']})
+        if not existing_article:
+            db.scopus.insert(article_dict, safe=True)
+        else:
+            print >> sys.stderr, 'The article already exists.'
+    page += 1
